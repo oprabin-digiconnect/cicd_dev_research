67221bd9afa8df85515cef58fdfd110e5d53d67c
oprabin <prabin.ojha@digiconnect.com.np>
2022-07-05 18:24:04 +0545

    Initial Commit.

---
+++ src/main/java/com/oprabin/cicd/CicdDevResearchApplication.java
@@ -0,0 +1,13 @@
+package com.oprabin.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDevResearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdDevResearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oprabin/cicd/CicdDevResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oprabin.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDevResearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
